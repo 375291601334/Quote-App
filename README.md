ff003136a09d6964fef5b50b8dfb9c60e35a3723
Anastasiya Hushcha <Anastasiya_Hushcha@epam.com>
2021-04-28 19:46:02 +0300

    feat: implement graceful shutdown

--- README.md
+++ README.md
@@ -24,3 +24,15 @@ File `.env` contains next environmental variables:
 | APP_PORT          | define an application port on which your server will keep connection and handle requests |
 | NODE_ENV          | identifies current application’s environment |
 | ENV_CONFIGURATION | identifies a name of current configuration that application server must use |
+
+<br />
+<br />
+
+### Graceful shutdown
+To test graceful shutdown you can start server and then press `<Ctrl>+C`.  
+You should see next messages in console:
+```sh
+SIGINT signal received !!!
+Closing http server.
+Http server closed.
+```
--- src/main.js
+++ src/main.js
@@ -14,8 +14,24 @@ app.get('/ping', ( req, res) => {
   res.json({ statusCode: 200, message: 'OK', time: process.uptime() });
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(
     `Quote app listening at http://localhost:${port} in ${process.env.NODE_ENV} mode`
   );
+});
+
+const sigs = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+sigs.forEach(sig => {
+  process.on(sig, () => {
+    console.info(`${sig} signal received !!!`);
+    console.log('Closing http server.');
+    server.close((err) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      console.log('Http server closed.');
+      process.exit(0);
+    });
+  });
 });
